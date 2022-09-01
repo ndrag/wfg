b1501b1599c80f3a056dda4d7851bbdec3101f9f
ndrag <ndraguno@gmail.com>
2022-09-01 12:12:38 -0700

    Check-in desired public files.

--- resources/js/Pages/Home.vue
+++ resources/js/Pages/Home.vue
@@ -3,6 +3,6 @@
     <Head>
       <title>{{ $page.props.title }}</title>
     </Head>
-    <h1>This is ABSOLUTELY: {{ $page.props.title }}</h1>
+    <h1>{{ $page.props.title }} now has a working CI/CD build process :)</h1>
   </span>
 </template>
