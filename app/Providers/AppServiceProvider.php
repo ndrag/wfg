45694c76fac523b9b01ab71c630765d82e6143cf
ndrag <ndraguno@gmail.com>
2022-09-01 20:06:41 -0700

    Force HTTPS asset loading on production.

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,6 +2,7 @@
 
 namespace App\Providers;
 
+use Illuminate\Support\Facades\URL;
 use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
@@ -23,6 +24,9 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot()
     {
-        //
+        # Force HTTPS for all calls to assets and external links. Shouldn't strictly be necessary if app environment is set to 'production', but this is a quick win solution! 
+        if(config('app.env') === 'production') {
+            URL::forceScheme('https');
+        }
     }
 }
