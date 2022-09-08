f5f1594f787263e21e6768668be253457b420697
ndrag <ndraguno@gmail.com>
2022-09-08 12:56:33 -0700

    Added a placeholder favicon

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -1,9 +1,26 @@
-<!DOCTYPE html>
+<!doctype html>
+
 <html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
 
 <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <title>Works for Good</title>
+    <!-- <meta name="description" content="Placeholder"> -->
+    <meta name="author" content="Nick Dragunow">
+
+    <meta property="og:title" content="Works for Good">
+    <meta property="og:type" content="website">
+    <meta property="og:url" content="https://www.worksforgood.info">
+    <!-- <meta property="og:description" content="Placeholder"> -->
+    <!-- <meta property="og:image" content="placeholder.png"> -->
+
+    <link rel="icon" href="/favicon.ico">
+    <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png">
+    <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png">
+    <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png">
+    <link rel="manifest" href="/app.webmanifest">
 
     @routes
     <link href="{{ asset(mix('css/app.css')) }}" rel="stylesheet">
@@ -11,10 +28,12 @@
     <script src="{{ asset(mix('js/vendor.js')) }}" defer></script>
     <script src="{{ asset(mix('js/app.js')) }}" defer></script>
     @inertiaHead
+
 </head>
 
 <body>
     @inertia
 </body>
+
 
 </html>
