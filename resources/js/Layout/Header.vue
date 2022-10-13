f58805a7b161f02b9c9d3a1e8144fdb43c71a247
ndrag <ndraguno@gmail.com>
2022-10-13 16:57:20 -0700

    Header, favicon, and logo styling.

--- resources/js/Layout/Header.vue
+++ resources/js/Layout/Header.vue
@@ -1,28 +1,30 @@
 <template>
-    <Disclosure as="nav" class="bg-gray-800 fixed w-full z-40" v-slot="{ open }">
+    <Disclosure as="nav" class="bg-gray-200 fixed w-full z-40" v-slot="{ open }">
         <div class="mx-auto max-w-7xl px-2 sm:px-6 lg:px-8">
             <div class="relative flex h-16 items-center justify-between">
+                <!-- Mobile menu button-->
                 <div class="absolute inset-y-0 left-0 flex items-center sm:hidden">
-                    <!-- Mobile menu button-->
                     <DisclosureButton
-                        class="inline-flex items-center justify-center rounded-md p-2 text-gray-400 hover:bg-gray-700 hover:text-white focus:outline-none focus:ring-2 focus:ring-inset focus:ring-white">
+                        class="header-link inline-flex items-center justify-center p-2">
                         <span class="sr-only">Open main menu</span>
                         <Bars3Icon v-if="!open" class="block h-6 w-6" aria-hidden="true" />
                         <XMarkIcon v-else class="block h-6 w-6" aria-hidden="true" />
                     </DisclosureButton>
                 </div>
                 
+                <!-- Favicon & desktop nav -->
                 <div class="flex flex-1 items-center justify-center sm:items-stretch sm:justify-start">
                     <div class="flex flex-shrink-0 items-center">
                         <img class="block h-8 w-auto"
-                            src="/works-for-good-logo.svg"
+                            src="/works-for-good-logo-gray.svg"
                             alt="Works for Good" />
                     </div>
                     <div class="hidden sm:ml-6 sm:block">
                         <div class="flex space-x-4">
                             <a v-for="item in navigation" :key="item.name" :href="item.href"
-                                class="text-gray-300 hover:bg-gray-700 hover:text-white px-3 py-2 rounded-md text-sm font-medium">{{
-                                item.name }}</a>
+                                class="header-link px-3 py-2 text-sm font-medium">
+                                {{item.name }}
+                            </a>
                         </div>
                     </div>
                 </div>
@@ -30,11 +32,11 @@
             </div>
         </div>
 
-        <!-- Large screen menu -->
+        <!-- Mobile panel -->
         <DisclosurePanel class="sm:hidden">
             <div class="space-y-1 px-2 pt-2 pb-3">
                 <DisclosureButton v-for="item in navigation" :key="item.name" as="a" :href="item.href"
-                    class="text-gray-300 hover:bg-gray-700 hover:text-white block px-3 py-2 rounded-md text-base font-medium">
+                    class="header-link block px-3 py-2 text-base font-medium">
                     {{ item.name }}</DisclosureButton>
             </div>
         </DisclosurePanel>
--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -1,6 +1,6 @@
 <!doctype html>
 
-<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}" class="text-gray-600">
 
 <head>
     <meta charset="utf-8">
