a99ad5675f885747e1259f5e5d517d074c19f658
ndrag <ndraguno@gmail.com>
2022-10-13 17:57:03 -0700

    Add a dev route to the live site.

--- routes/web.php
+++ routes/web.php
@@ -14,3 +14,15 @@ Route::get(
         );
     }
 )->name( 'homepage' );
+
+Route::get(
+    '/development-view',
+    function () {
+        return Inertia::render(
+            'Home',
+            [
+                'title' => 'Works for Good',
+            ]
+        );
+    }
+)->name( 'dev' );
