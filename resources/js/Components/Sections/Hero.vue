47f756d0f94ed74fce3620877a6821cada3cbb1c
ndrag <ndraguno@gmail.com>
2022-10-13 23:12:12 -0700

    Title font is now bold.

--- resources/js/Components/Sections/Hero.vue
+++ resources/js/Components/Sections/Hero.vue
@@ -5,7 +5,7 @@
             <main class="mx-auto max-w-7xl px-4 sm:mt-24">
                 <div class="text-center">
                     <h1 class="text-5xl sm:text-6xl md:text-7xl">
-                        <span class="block xl:inline">We work for good.</span>
+                        <span class="block font-bold">We work for good.</span>
                     </h1>
                     <p class="mt-6 sm:mt-3 md:mt-5 text-md sm:text-md md:text-xl max-w-md md:max-w-3xl">
                         We build websites, for free, where you help us find the need.</p>
