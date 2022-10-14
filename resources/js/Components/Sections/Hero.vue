376b5bbd1df603e4ea16bc604b7728ac2498105e
ndrag <ndraguno@gmail.com>
2022-10-13 22:03:21 -0700

    Made text in the hero section more responsive.

--- resources/js/Components/Sections/Hero.vue
+++ resources/js/Components/Sections/Hero.vue
@@ -4,10 +4,10 @@
         <div class="relative pt-6 pb-16 sm:pb-24">
             <main class="mx-auto max-w-7xl px-4 sm:mt-24">
                 <div class="text-center">
-                    <h1 class="text-4xl tracking-tight sm:text-5xl md:text-6xl">
+                    <h1 class="text-5xl sm:text-6xl md:text-7xl">
                         <span class="block xl:inline">We work for good.</span>
                     </h1>
-                    <p class="mx-auto mt-3 max-w-md text-base sm:text-lg md:mt-5 md:max-w-3xl md:text-xl">
+                    <p class="xs-mt-3 mt-10 sm:text-lg md:mt-5 text-xl md:text-2xl max-w-md md:max-w-3xl">
                         We build websites, for free, where you help us find the need.</p>
                 </div>
             </main>
