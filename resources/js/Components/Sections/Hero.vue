5bf45d30e63929c3ddc27edb64af8c1b5145fc4b
ndrag <ndraguno@gmail.com>
2022-10-14 00:27:07 -0700

    Increased hero font size on mobile.

--- resources/js/Components/Sections/Hero.vue
+++ resources/js/Components/Sections/Hero.vue
@@ -4,10 +4,10 @@
         <div class="relative pt-6 pb-16 sm:pb-24">
             <main class="mx-auto max-w-7xl px-4">
                 <div class="text-center ">
-                    <h1 class="text-5xl sm:text-6xl md:text-7xl">
+                    <h1 class="text-8xl sm:text-6xl md:text-7xl">
                         <span class="block font-bold text-gray-100">We work for good.</span>
                     </h1>
-                    <p class="leading-10 mt-6 sm:mt-3 md:mt-5 text-md sm:text-md md:text-xl max-w-md md:max-w-3xl text-gray-200">
+                    <p class="leading-10 mt-16 sm:mt-3 md:mt-5 text-2xl sm:text-md md:text-xl max-w-3xl text-gray-200">
                         We build websites <span class="font-bold whitespace-nowrap p-1 border-2 rounded-sm">for free</span> where you help us find the need.</p>
                 </div>
             </main>
