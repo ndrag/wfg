b22e30e7f6422a4fae26295355d4407d2b72e206
ndrag <ndraguno@gmail.com>
2022-10-13 22:11:20 -0700

    Further text size & margin responsiveness updates.

--- resources/js/Components/Sections/Hero.vue
+++ resources/js/Components/Sections/Hero.vue
@@ -7,7 +7,7 @@
                     <h1 class="text-5xl sm:text-6xl md:text-7xl">
                         <span class="block xl:inline">We work for good.</span>
                     </h1>
-                    <p class="xs-mt-3 mt-10 sm:text-lg md:mt-5 text-xl md:text-2xl max-w-md md:max-w-3xl">
+                    <p class="mt-6 sm:mt-3 md:mt-5 text-md sm:text-md md:text-xl max-w-md md:max-w-3xl">
                         We build websites, for free, where you help us find the need.</p>
                 </div>
             </main>
