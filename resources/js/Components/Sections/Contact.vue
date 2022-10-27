0367622dcd1282706d64e0106ace978675ec7c0a
ndrag <ndraguno@gmail.com>
2022-10-26 17:16:24 -0700

    Seamlessly hide the recaptcha field on successful contact form submission.

--- resources/js/Components/Sections/Contact.vue
+++ resources/js/Components/Sections/Contact.vue
@@ -91,7 +91,7 @@
                             <div v-if="Form.errors.message" class="mt-1 text-red-500">{{ Form.errors.message }}</div>
                         </div>
                     </div>
-                    <div class="sm:col-span-2" :class="Submitted ? 'h-24' : ''">
+                    <div class="sm:col-span-2" :class="Submitted ? 'h-24 pt-2' : ''">
                         <VueRecaptcha v-if="!Submitted" class="h-24 pt-2" :disabled="Submitted || Form.processing"
                             id="recaptcha" ref="recaptcha" :sitekey=SiteKey :load-recaptcha-script="true"
                             @verify="handleRecaptchaValidation" @expired="CanSubmit = false">
