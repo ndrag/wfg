0049c8c075f6d0e89cab852f41753c0f185c72ff
ndrag <ndraguno@gmail.com>
2022-10-30 17:11:50 -0700

    Fixed section numbers having incorrect padding on mobile browsers.

--- resources/js/Components/SectionNumber.vue
+++ resources/js/Components/SectionNumber.vue
@@ -1,5 +1,5 @@
 <template>
-    <div class="mx-auto pl-0 rounded-full w-16 h-16 opacity-80 text-center bg-wfg-blue bg-gradient-to-b from-wfg-violet align-middle text-white text-4xl font-bold=" style="line-height: 3.8rem">
+    <div class="mx-auto pl-0 rounded-full w-[96px] h-[96px] opacity-80 text-center bg-wfg-blue bg-gradient-to-b from-wfg-violet align-middle text-white text-[54px] leading-[90px] font-bold">
         <span>{{ value }}</span>
     </div>
 </template>
