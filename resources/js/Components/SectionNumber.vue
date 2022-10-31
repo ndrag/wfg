1cd09d3c4d5a68de93d6f5766623cc4fa59aba16
ndrag <ndraguno@gmail.com>
2022-10-30 18:21:18 -0700

    Fix mobile misalignment of section numbers.

--- resources/js/Components/SectionNumber.vue
+++ resources/js/Components/SectionNumber.vue
@@ -1,5 +1,5 @@
 <template>
-    <div class="mx-auto pl-0 rounded-full w-[85px] h-[85px] opacity-80 text-center bg-wfg-blue bg-gradient-to-b from-wfg-violet align-middle text-white text-[54px] leading-[80px] font-bold">
+    <div class="mx-auto pl-0 rounded-full w-[85px] h-[85px] text-[54px] leading-[88px] sm:leading-[80px] opacity-80 text-center bg-wfg-blue bg-gradient-to-b from-wfg-violet align-middle text-white font-bold">
         <span>{{ value }}</span>
     </div>
 </template>
